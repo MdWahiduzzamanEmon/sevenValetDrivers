c87e7f43aad6b8fd88c903d036aaa1831a972458
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2025-05-05 19:32:24 +0600

    fix: update pushService URL to use the correct endpoint

--- src/Base/index.ts
+++ src/Base/index.ts
@@ -1,6 +1,7 @@
 const apiUrl = {
   master: '',
-  pushService: 'http://10.0.2.2:4500/api/push-service/send-fcm-token',
+  // pushService: 'http://10.0.2.2:4500/api/push-service/send-fcm-token',
+  pushService: 'http://31.220.6.150:1221/api/push-service/send-fcm-token',
   // `http://localhost:4500/api/push-service/send-fcm-token`,
 };
 
