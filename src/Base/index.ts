a1c8acee1cb1ed646a124fa7d737b222e5d33d33
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2025-07-25 12:59:48 +0600

    feat: Update push service URL in apiUrl and add tsconfig.json for Android

--- src/Base/index.ts
+++ src/Base/index.ts
@@ -1,7 +1,7 @@
 const apiUrl = {
   master: 'https://www.zohoapis.com/creator/custom/zied_sevenofficial/',
-  // pushService: 'http://10.0.2.2:4500/api/push-service/send-fcm-token',
-  pushService: 'http://31.220.6.150:1221/api/push-service/send-fcm-token',
+  // pushService: 'http://31.220.6.150:1221/api/push-service/send-fcm-token',
+  pushService: 'http://128.199.80.102:1221/api/push-service/send-fcm-token',
   // `http://localhost:4500/api/push-service/send-fcm-token`,
 };
 
